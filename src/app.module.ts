f50c2ce08f955e0f881f5df458ef946544c1a664
Higor Muros <96095355+higormuros@users.noreply.github.com>
2022-09-13 17:34:43 -0300

    Projeto finalizado em funcionando

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TaskService } from './tasks/shared/task.service';
+import { TasksController } from './tasks/tasks.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController,TasksController],
+  providers: [AppService,TaskService],
+})
+export class AppModule {}
